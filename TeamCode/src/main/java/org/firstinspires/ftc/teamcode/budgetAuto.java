6a540f0b3a54a44166cb378454e2e2951876add6
VenomSoftwareLucca <lucca1belli@gmail.com>
2025-11-15 06:10:57 -0600

    Create budgetAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/budgetAuto.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+@Autonomous(name = "budgetAuto", group = "autos")
+public class budgetAuto extends LinearOpMode{
+
+    private DcMotor frontRightMotor;
+    private DcMotor frontLeftMotor;
+    private DcMotor backRightMotor;
+    private DcMotor backLeftMotor;
+
+    public void runOpMode() throws InterruptedException{
+
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        double time = System.nanoTime() / 1E9;
+
+        while (time < 2){
+
+            frontRightMotor.setPower(.25);
+            frontLeftMotor.setPower(.25);
+            backRightMotor.setPower(.25);
+            backLeftMotor.setPower(.25);
+            time = System.nanoTime() / 1E9;
+        }
+
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+
+    }
+
+}
